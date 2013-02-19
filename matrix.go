314bbd44045f7db606a7481cbdbd012c08d7a492
Siva Manoharan <smanoharan@outlook.com>
2013-02-20 01:05:25 +1300

    Added: dot and cross products; matrix product

--- matrix.go
+++ matrix.go
@@ -11,6 +11,14 @@ const (
 	V4LEN = 4
 )
 
+// define names for Vector ordinates
+const (
+	cX = 0
+	cY = 1
+	cZ = 2
+	cW = 3
+)
+
 type (
 	entry float64      // each element of a matrix/vector
 	Mat3  [M3LEN]entry // 3x3 matrix
@@ -19,86 +27,170 @@ type (
 	Vec4  [V4LEN]entry // 4D vector
 )
 
+// addition: add slices m1 and m2, each with n entries. result is placed in m3. 
+func add(m1, m2, m3 []entry, n int) {
+	for i := 0; i < n; i++ {
+		m3[i] = m1[i] + m2[i]
+	}
+}
+
+// dot product: 3-vectors
+func (m *Vec3) dot(n *Vec3) *Vec3 {
+	return &Vec3{
+		m[cX] * n[cX],
+		m[cY] * n[cY],
+		m[cZ] * n[cZ],
+	}
+}
+
+// dot product: 4-vectors
+func (m *Vec4) dot(n *Vec4) *Vec4 {
+	return &Vec4{
+		m[cX] * n[cX],
+		m[cY] * n[cY],
+		m[cZ] * n[cZ],
+		m[cW] * n[cW],
+	}
+}
+
+// cross product: (only defined for) 3-vectors
+func (m *Vec3) cross(n *Vec3) *Vec3 {
+	return &Vec3{
+		m[cY]*n[cZ] - n[cY]*m[cZ],
+		m[cZ]*n[cX] - n[cZ]*m[cX],
+		m[cX]*n[cY] - n[cX]*m[cY],
+	}
+}
+
+// scalar product: matrix (m1) with n entries by a scalar s, result m2
+func multScalar(m1, m2 []entry, n int, s entry) {
+	for i := 0; i < n; i++ {
+		m2[i] = m1[i] * s
+	}
+}
+
+// multiplication: axn matrix (m1)  by nxb matrix (m2) and 
+// place the result into axb matrix (m3)
+func mult(m1, m2, m3 []entry, a, n, b int) {
+
+	// iterate through rows of m1
+	for row := 0; row < a; row++ {
+		ro := row * n // row-offset
+
+		// iterate through cols of m2
+		for col := 0; col < b; col++ {
+
+			// compute m1[row] dot-product m2[col]
+			sum := entry(0)
+			for i := 0; i < n; i++ {
+				sum += m1[ro+i] * m2[i*b+col]
+			}
+			m3[row*b+col] = sum
+		}
+	}
+}
+
+// Wrappers for each of the types:
+
 // addition: 3x3 matrices
 func (m *Mat3) plus(n *Mat3) *Mat3 {
 	res := new(Mat3)
-	for i := 0; i < M3LEN; i++ {
-		res[i] = m[i] + n[i]
-	}
+	add(m[:], n[:], res[:], M3LEN)
 	return res
 }
 
 // addition: 4x4 matrices
 func (m *Mat4) plus(n *Mat4) *Mat4 {
 	res := new(Mat4)
-	for i := 0; i < M4LEN; i++ {
-		res[i] = m[i] + n[i]
-	}
+	add(m[:], n[:], res[:], M4LEN)
 	return res
 }
 
 // addition: 3-vectors
 func (m *Vec3) plus(n *Vec3) *Vec3 {
 	res := new(Vec3)
-	for i := 0; i < V3LEN; i++ {
-		res[i] = m[i] + n[i]
-	}
+	add(m[:], n[:], res[:], V3LEN)
 	return res
 }
 
 // addition: 4-vectors
 func (m *Vec4) plus(n *Vec4) *Vec4 {
 	res := new(Vec4)
-	for i := 0; i < V4LEN; i++ {
-		res[i] = m[i] + n[i]
-	}
+	add(m[:], n[:], res[:], V4LEN)
+	return res
+}
+
+// scalar product for 3-vectors
+func (m *Vec3) scale(s entry) *Vec3 {
+	res := new(Vec3)
+	multScalar(m[:], res[:], V3LEN, s)
+	return res
+}
+
+// scalar product for 4-vectors
+func (m *Vec4) scale(s entry) *Vec4 {
+	res := new(Vec4)
+	multScalar(m[:], res[:], V4LEN, s)
+	return res
+}
+
+// scalar product for 3x3 matrices
+func (m *Mat3) scale(s entry) *Mat3 {
+	res := new(Mat3)
+	multScalar(m[:], res[:], M3LEN, s)
+	return res
+}
+
+// scalar product for 4x4 matrices
+func (m *Mat4) scale(s entry) *Mat4 {
+	res := new(Mat4)
+	multScalar(m[:], res[:], M4LEN, s)
 	return res
 }
 
 // multiplication: 3x3 matrices
 func (m *Mat3) times(n *Mat3) *Mat3 {
 	res := new(Mat3)
-
-	for row := 0; row < V3LEN; row++ {
-
-		ri := row * V3LEN                        // row index
-		mr1, mr2, mr3 := m[ri], m[ri+1], m[ri+2] // get row from m
-
-		for col := 0; col < V3LEN; col++ {
-
-			nc1, nc2, nc3 := n[col], n[col+V3LEN], n[col+2*V3LEN] // get col from n
-			res[row*V3LEN+col] = mr1*nc1 + mr2*nc2 + mr3*nc3
-		}
-	}
-
+	mult(m[:], n[:], res[:], V3LEN, V3LEN, V3LEN)
 	return res
 }
 
 // multiplication: 4x4 matrices
 func (m *Mat4) times(n *Mat4) *Mat4 {
 	res := new(Mat4)
-
-	for row := 0; row < V3LEN; row++ {
-
-		ri := row * V3LEN                        // row index
-		mr1, mr2, mr3 := m[ri], m[ri+1], m[ri+2] // get row from m
-
-		for col := 0; col < V3LEN; col++ {
-
-			nc1, nc2, nc3 := n[col], n[col+V3LEN], n[col+2*V3LEN] // get col from n
-			res[row*V3LEN+col] = mr1*nc1 + mr2*nc2 + mr3*nc3
-		}
-	}
-
+	mult(m[:], n[:], res[:], V4LEN, V4LEN, V4LEN)
 	return res
 }
 
-func add(a, b entry) entry {
-	return a + b
+// multiplication: 3-vec * 3x3 mat
+func (v *Vec3) timesMat(m *Mat3) *Vec3 {
+	res := new(Vec3)
+	mult(v[:], m[:], res[:], 1, V3LEN, V3LEN)
+	return res
+}
+
+// multiplication: 3x3 mat * 3-vec
+func (m *Mat3) timesVec(v *Vec3) *Vec3 {
+	res := new(Vec3)
+	mult(m[:], v[:], res[:], V3LEN, V3LEN, 1)
+	return res
+}
+
+// multiplication: 4-vec * 4x4 mat
+func (v *Vec4) timesMat(m *Mat4) *Vec4 {
+	res := new(Vec4)
+	mult(v[:], m[:], res[:], 1, V4LEN, V4LEN)
+	return res
+}
+
+// multiplication: 4x4 mat * 4-vec
+func (m *Mat4) timesVec(v *Vec4) *Vec4 {
+	res := new(Vec4)
+	mult(m[:], v[:], res[:], V4LEN, V4LEN, 1)
+	return res
 }
 
 func main() {
-	// TODO fix
-	print(add(entry(1), entry(2)))
+	// TODO
 	print("\n")
 }
--- matrix_test.go
+++ matrix_test.go
@@ -11,15 +11,21 @@ import (
 //		- Vec3, Vec4
 //		- Mat3, Mat4
 //  - multiplication tests
-//		- TODO
+//		- scalar (Vec3, Vec4, Mat3, Mat4)
+//		- dot (Vec3, Vec4), cross (Vec3) 
+//		- Mat3*Mat3, Mat4*Mat4 
+//		- Vec3*Mat3, Mat3*Vec3, Vec4*Mat4, Vec4*Mat4 
 
 var (
-	v31, v32 Vec3 = Vec3{4, 7, 112}, Vec3{1, 2, -11}
-	v41, v42 Vec4 = Vec4{-10, 5, 112, 18}, Vec4{144, 3, -10, 22}
-	m31      Mat3 = Mat3{11, 12, 13, 21, 22, 23, 31, 32, 33}
-	m32      Mat3 = Mat3{44, 45, 46, 33, 30, 27, 51, 42, 33}
-	m41      Mat4 = Mat4{11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44}
-	m42      Mat4 = Mat4{44, 45, 46, 47, 33, 30, 27, 24, 51, 42, 33, 24, 11, 21, 13, 41}
+	// some example matrices and vectors, for testing
+	v31 Vec3 = Vec3{4, 7, 112}
+	v32 Vec3 = Vec3{1, 2, -11}
+	v41 Vec4 = Vec4{-10, 5, 112, 18}
+	v42 Vec4 = Vec4{144, 3, -10, 22}
+	m31 Mat3 = Mat3{11, 12, 13, 21, 22, 23, 31, 32, 33}
+	m32 Mat3 = Mat3{44, 45, 46, 33, 30, 27, 51, 42, 33}
+	m41 Mat4 = Mat4{11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44}
+	m42 Mat4 = Mat4{44, 45, 46, 47, 33, 30, 27, 24, 51, 42, 33, 24, 11, 21, 13, 41}
 )
 
 func TestAdditionOf3DVectors(t *testing.T) {
@@ -52,6 +58,83 @@ func TestAdditionOf4x4Matrices(t *testing.T) {
 	// check both addition directions
 	assertEquals(t, expM, *m41.plus(&m42), "Mat4 addition 1+2")
 	assertEquals(t, expM, *m42.plus(&m41), "Mat4 addition 2+1")
+}
+
+func TestScalarMultiplicationOf3DVectors(t *testing.T) {
+	assertEquals(t, Vec3{8, 14, 224}, *v31.scale(2), "Vec3 mult by 2")
+	assertEquals(t, Vec3{-1, -1.75, -28}, *v31.scale(-0.25), "Vec3 mult by -0.25")
+}
+
+func TestScalarMultiplicationOf4DVectors(t *testing.T) {
+	assertEquals(t, Vec4{-30, 15, 336, 54}, *v41.scale(3), "Vec4 mult by 3")
+	assertEquals(t, Vec4{5, -2.5, -56, -9}, *v41.scale(-0.5), "Vec4 mult by -0.5")
+}
+
+func TestScalarMultiplicationOf3x3Matrices(t *testing.T) {
+	exp1m := Mat3{44, 48, 52, 84, 88, 92, 124, 128, 132}       // m31*4
+	exp2m := Mat3{-11, -12, -13, -21, -22, -23, -31, -32, -33} // m31 * -1.0
+	assertEquals(t, exp1m, *m31.scale(4), "Mat3 mult by 4")
+	assertEquals(t, exp2m, *m31.scale(-1.0), "Mat3 mult by -1.0")
+}
+
+func TestScalarMultiplicationOf4x4Matrices(t *testing.T) {
+	exp1m := Mat4{110, 120, 130, 140, 210, 220, 230, 240, 310, 320, 330, 340, 410, 420, 430, 440}      // m41*10
+	exp2m := Mat4{2.75, 3, 3.25, 3.5, 5.25, 5.50, 5.75, 6, 7.75, 8, 8.25, 8.5, 10.25, 10.5, 10.75, 11} // m41*0.25
+	assertEquals(t, exp1m, *m41.scale(10), "Mat3 mult by 10")
+	assertEquals(t, exp2m, *m41.scale(0.25), "Mat3 mult by 0.25")
+}
+
+func TestDotProductOf3DVectors(t *testing.T) {
+	expV := Vec3{4, 14, -1232} // v31 . v32
+
+	// check both dot-prod directions
+	assertEquals(t, expV, *v31.dot(&v32), "Vec3 dot 1.2")
+	assertEquals(t, expV, *v32.dot(&v31), "Vec3 dot 2.1")
+}
+
+func TestDotProduct4DVectors(t *testing.T) {
+	expV := Vec4{-1440, 15, -1120, 396} // v41 . v42
+
+	// check both dot-prod directions
+	assertEquals(t, expV, *v41.dot(&v42), "Vec4 dot 1.2")
+	assertEquals(t, expV, *v42.dot(&v41), "Vec4 dot 2.1")
+}
+
+func TestCrossProduct(t *testing.T) {
+	assertEquals(t, Vec3{-301, 156, 1}, *v31.cross(&v32), "Vec3 cross 1x2")
+	assertEquals(t, Vec3{301, -156, -1}, *v32.cross(&v31), "Vec3 cross 2x1")
+
+	// extra test: X-axis cross Y-axis should be Z-axis
+	xAx, yAx, zAx := Vec3{1, 0, 0}, Vec3{0, 1, 0}, Vec3{0, 0, 1}
+	assertEquals(t, zAx, *xAx.cross(&yAx), "Vec3 cross XxY")
+}
+
+func TestMultiplicationOf3x3Matrices(t *testing.T) {
+	exp1m := Mat3{1543, 1401, 1259, 2823, 2571, 2319, 4103, 3741, 3379} // m31 x m32
+	exp2m := Mat3{2855, 2990, 3125, 1830, 1920, 2010, 2466, 2592, 2718} // m32 x m31
+	assertEquals(t, exp1m, *m31.times(&m32), "Mat3 mult 1x2")
+	assertEquals(t, exp2m, *m32.times(&m31), "Mat3 mult 2x1")
+}
+
+func TestMultiplicationOf4x4Matrices(t *testing.T) {
+	exp1m := Mat4{1697, 1695, 1441, 1691, 3087, 3075, 2631, 3051, 4477, 4455, 3821, 4411, 5867, 5835, 5011, 5771} // m41 x m42
+	exp2m := Mat4{4782, 4964, 5146, 5328, 2814, 2928, 3042, 3156, 3450, 3600, 3750, 3900, 2646, 2732, 2818, 2904} // m42 x m41
+	assertEquals(t, exp1m, *m41.times(&m42), "Mat4 mult 1x2")
+	assertEquals(t, exp2m, *m42.times(&m41), "Mat4 mult 2x1")
+}
+
+func TestMultiplicationOf3VectorAndMatrix(t *testing.T) {
+	exp1v := Vec3{1584, 2814, 4044} // m31 * v31
+	exp2v := Vec3{3663, 3786, 3909} // v31 * m31
+	assertEquals(t, exp1v, *m31.timesVec(&v31), "Vec3 mult Mat3")
+	assertEquals(t, exp2v, *v31.timesMat(&m31), "Mat3 mult Vec3")
+}
+
+func TestMultiplicationOf4VectorAndMatrix(t *testing.T) {
+	exp1v := Vec4{1658, 2908, 4158, 5408} // v41 * m41
+	exp2v := Vec4{4205, 4330, 4455, 4580} // m41 * v41
+	assertEquals(t, exp1v, *m41.timesVec(&v41), "Vec4 mult Mat4")
+	assertEquals(t, exp2v, *v41.timesMat(&m41), "Mat4 mult Vec4")
 }
 
 // Helper functions (for checking equality, with error messages)
