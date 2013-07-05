ea1dc5bb8ba0aab63056be51d7e4b935845b9041
Siva Manoharan <smanoharan@outlook.com>
2013-07-06 01:25:08 +1200

    Added Usage instructions to README

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 )
 
 func main() {
+	sampleScene1()
+}
+
+func sampleScene1() {
 
 	// camera at (0,1,5) looking towards origin
 	eyePos, lookAt, up := Vec3{0, 2, 6}, ZERO_V3, Y_V3
-	width, height, fovY := 300, 300, entry(50)
+	width, height, fovY := 1280, 800, entry(50)
 	view := &Camera{eyePos, lookAt, up, width, height, fovY}
 
 	// init ray tracer
@@ -24,6 +28,7 @@ func main() {
 		&PointLight{Vec3{0.2, 0.4, 0.2}, Vec3{0, 5, 3}, atten},
 		&PointLight{Vec3{0.4, 0.3, 0.3}, Vec3{-6, 1, 3}, atten},
 	}
+
 	//	scene1 := []Shape{NewSphere(TWO, &ZERO_V3, mat), NewSphere(ONE, X_V3.scale(FOUR), mat)}
 	s2size := 5
 	scene2 := make([]Shape, s2size*s2size)
